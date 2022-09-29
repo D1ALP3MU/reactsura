5c44e26c479837f43a86f4e4e8b07bbb30fcb5e8
D1ALP3MU <diegoalejandroperezm@gmail.com>
2022-09-29 17:23:13 -0500

    Se crea nuevo proyecto con react

---
+++ src/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+export function Home(){
+
+    return(
+        <>
+            <section class="banner text-white">
+                <h1 class="fw-bold">RYAN CASTRO</h1>
+                <p>Bryan Castro Sosa (14 de enero de 1994), más conocido como Ryan Castro, es un cantante y compositor colombiano del género urbano. En 2017 incursionó en la música con el sencillo Morena. Más tarde se dio a conocer apadrinado por Kevin Roldan. Entre sus canciones se destacan: Dime, 1994, Lejanía, Chupable y Medellín. En la actualidad, Castro es considerado una de las nuevas caras del llamado reggaetón de barrio.</p>
+            </section>
+        </>
+    )
+}
